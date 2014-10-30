27594784b717103bc8ebcd771bc879bd9b9175ea
Kasjan Siwek <kasjan.siwek@gmail.com>
2014-10-30 18:37:48 +0100

    Cleared up the code

--- pascal.c
+++ pascal.c
@@ -23,28 +23,30 @@ int main (int argc, char *argv[])
         fatal("n has to be at least 1!");
         exit(0);
     }
+
+    // 2n lacz, ze wzgledu na komunikacje dwukierunkowa
     int pipe_dsc[2*n][2];
 
     int i;
     for (i = 0; i < 2*n; ++i) {
-        if (pipe (pipe_dsc[i]) == -1) syserr("Error in pipe\n");
+        if (pipe (pipe_dsc[i]) == -1) 
+            syserr("Failed to create a pipe\n");
     }
 
 
     for (i = 0; i < n; ++i) {
         switch (fork ()) {
             case -1: 
-                syserr("Error in fork\n");
+                syserr("Failed to fork\n");
 
+            // Proces potomny
             case 0:
-                if (close (0) == -1)            syserr("child, close (%d)", i);
-                if (close (1) == -1)            syserr("child, close (%d)", i);
-//                fprintf(stderr, "Jestem procesem %d i bede uzywal %d jako stdin (koniec pisany %d)\n", i, pipe_dsc[i][0],
-//                        pipe_dsc[i][1]);
+                // Podmiana stdin i stdout
+                if (close (0) == -1)            syserr("Process %i, failed to close(0)", i);
+                if (close (1) == -1)            syserr("Process %i, failed to close(1)", i);
                 if (dup (pipe_dsc[i][0]) != 0)    syserr("child, dup (pipe_dsc [0])");
+                // W(n) nie ma W(n+1) do ktorego moglby napisac
                 if (i != n-1) {
-//                    fprintf(stderr, "Jestem procesem %d i bede uzywal %d jako stdout (koniec czytany %d)\n", i, pipe_dsc[i+1][1],
-//                        pipe_dsc[i+1][0]);
                     if (dup (pipe_dsc[i+1][1]) != 1)    syserr("child, dup (pipe_dsc [1])");
                 }
 
@@ -53,44 +55,15 @@ int main (int argc, char *argv[])
                     if (j != i) {
                         if (close (pipe_dsc[j][0]) == -1) syserr("child %d, close (pipe_dsc[%d][0])", i, j);
                         if (close (pipe_dsc[n+j][1]) == -1) syserr("child %d, close (pipe_dsc[n+%d][0])", i, j);
+                        // Ostatni proces nie ma lacz 'za soba', ktore moglby zamknac
                         if (j != n-1) {
                             if (close (pipe_dsc[j+1][1]) == -1) syserr("child %d, close (pipe_dsc[%d+1][1])", i, j);
                             if (close (pipe_dsc[n+j+1][0]) == -1) syserr("child %d, close (pipe_dsc[n+%d+1][1]", i, j);
                         }
                     }
                 }
-/*
-                char str[10];
-                fgets(str, 10, stdin);
-                int aux = atoi(str);
-                fprintf(stderr, "Jestem procesem %d i odebralem %d\n", i, aux);
-                aux = aux + 1;
-                if (i != n-1) {
-                    printf("%d\n", aux);
-                    fprintf(stderr, "Jestem procesem %d i wypisalem %d\n", i, aux);
-                }
 
-                if ( i != n-1) {
-                    int len = read(pipe_dsc[n+i+1][0], str, 10);
-                    if (len == -1)
-                        syserr("child %d, failed read", i);
-                    aux = atoi(str);
-                    fprintf(stderr, "Jestem procesem %d i odebralem z prawej %d bajtow, %d\n", i, len, aux);
-                }
-
-                aux = aux + 1;
-                sprintf(str,"%d\n", aux);
-                if (i != 0) {
-                    int len = write(pipe_dsc[n+i][1], str, 10);
-                    if (len == -1)
-                        syserr("child %d, failed write", i);
-                    fprintf(stderr, "Jestem procesem %d i wyslalem na lewo %d bajtow, %d\n", i, len, aux);
-                }
-
-
-                return 0;
-                */
-
+                // Konwersja argumentow do stringow
                 char n_str[10];
                 sprintf(n_str,"%d", n);
                 char i_str[10];
@@ -99,22 +72,18 @@ int main (int argc, char *argv[])
                 sprintf(read_dsc, "%d", pipe_dsc[n+i+1][0]);
                 char write_dsc[10];
                 sprintf(write_dsc, "%d", pipe_dsc[n+i][1]);
-                fprintf(stderr, "Startuje execl z argumentami : %s, %s, %s, %s, %s, %s\n", subprocess_cmd,
-                        subprocess, n_str, i_str, read_dsc, write_dsc);
+
+                // Uruchomienie procesu W(i)
                 execl(subprocess_cmd, subprocess, n_str, i_str, read_dsc, write_dsc, (char *) 0);
                 syserr("Error in execl\n");
 
-        } /* switch (fork ()) */
+        }
     }
-/*
-    char str[10] = "1\n";
-    for (i = 0; i < n; ++i)
-        fprintf(stderr, "Jestem procesem macierzystym i wysylam '1' po raz %d\n", i);
-        write(pipe_dsc[0][1], str, 10);
-*/
+
+    // Czekamy na zakonczenie procesow potomnych
     for (i = 0; i < n; ++i) {
         if (wait (0) == -1) 
-            syserr("wait");
+            syserr("Waiting failed");
     }
     exit (0);
 }
--- process.c
+++ process.c
@@ -14,40 +14,29 @@ int main (int argc, char *argv[]) {
         exit(0);
     }
 
-//    fprintf(stderr, "Odebralem argumenty: %s, %s, %s, %s, %s\n", argv[0], argv[1], argv[2], argv[3], argv[4]);
-
     int n = atoi(argv[1]);
     int i = atoi(argv[2]);
+//    int read_dsc = atoi(argv[3]);
+//    int write_dsc = atoi(argv[4]);
 
-    int read_dsc, write_dsc, buf_len;
-    char buf [BUF_SIZE];
-
-    read_dsc = atoi(argv[3]);
-    write_dsc = atoi(argv[4]);
-
+    char buf[BUF_SIZE];
 
     int value = 0; 
     if (i == 0)
         value = 1;
+
     int j;
     
     for (j = 0; j < n - i; ++j) {    
+
         if (i != 0) {
-//            fprintf(stderr, "Jestem procesem %d i oczekuje na input po raz %d\n", i, j);
-            if (fgets(buf, 10, stdin) == NULL)
-                syserr("Failed to read");
-//            int len = read(0, buf, 10);
-//            if (len == -1) {
-//                syserr("Failed to read");
-//            }
-//            fprintf(stderr, "Jestem procesem %d i odczytalem '%s'\n", i, buf);
+            if (fgets(buf, BUF_SIZE, stdin) == NULL)
+                syserr("Process %d failed to read a value\n", i+1);
             int aux = atoi(buf);
             value += aux;
         }
+
         if (j != n - i - 1) {
-//            fprintf(stderr, "Jestem procesem %d i wysylam output po raz %d, tresc %d\n", i, j, value);
-            int len = sprintf(buf, "%d\n", value);
-//            write(1, buf, len);
             printf("%d\n", value);
         }
     }
